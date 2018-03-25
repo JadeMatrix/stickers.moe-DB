a116a386ce2e43a66f57608c93852c7fe654c106
JadeMatrix <jadematrix.art@gmail.com>
2018-03-25 16:21:49 -0400

    Added basic permissions schema

---
+++ sql/init_permissions_schema.sql
@@ -0,0 +1,37 @@
+-- PostgreSQL 9.6
+
+
+CREATE SCHEMA permissions
+    AUTHORIZATION postgres
+;
+
+
+-- TABLES ----------------------------------------------------------------------
+
+
+CREATE TABLE permissions.permissions
+    (
+        permission_id   util.BIGID PRIMARY KEY DEFAULT util.next_nonseq_id(
+                            'permissions.permissions',
+                            'permission_id'
+                        ),
+        permission      TEXT UNIQUE NOT NULL
+    )
+;
+
+
+CREATE TABLE permissions.user_permissions
+    (
+        user_id         util.BIGID NOT NULL
+                        REFERENCES users.user_core ( user_id ) MATCH FULL
+                            ON DELETE CASCADE
+                            ON UPDATE CASCADE,
+        permission      util.BIGID NOT NULL
+                        REFERENCES permissions.permissions ( permission_id )
+                            MATCH FULL
+                            ON DELETE CASCADE
+                            ON UPDATE CASCADE,
+        
+        UNIQUE ( user_id, permission )
+    )
+;
